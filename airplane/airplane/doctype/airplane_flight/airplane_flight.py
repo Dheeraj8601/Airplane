1e4c7fa9959ce96b41509ef9703d3577d909122b
Dheeraj s <dheerajs862001@gmail.com>
2024-05-03 15:31:27 +0530

    second

--- airplane/airplane/doctype/airplane_flight/airplane_flight.py
+++ airplane/airplane/doctype/airplane_flight/airplane_flight.py
@@ -1,15 +1,30 @@
 # Copyright (c) 2024, Dheeraj S and contributors
 # For license information, please see license.txt
-from frappe.model.document import Document
+#from frappe.website.website_generator import WebsiteGenerator
+
 
 import frappe
-
+from frappe.model.document import Document
 
 # from frappe.website.website_generator import WebsiteGenerator
 from frappe.website.website_generator import WebsiteGenerator
 
 
 class AirplaneFlight(Document):
+
+    def get_page_info(self):
+        # Define logic to gather information for rendering the page
+        page_info = {
+            "title": self.airplane,
+            "content": {
+                "source_airport_code": self.source_airport_code,
+                "destination_airport_code": self.destination_airport_code,
+                "departure_info": f"{self.date_of_departure} | {self.time_of_departure}",
+                "duration": frappe.utils.format_duration(self.duration)
+            }
+        }
+        return page_info
+
     def validate(self):
         self.check_source_and_destination_airport()
         self.set_flight_status()
