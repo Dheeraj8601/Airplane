6f629df4d8ff533ad1e4916ba262d216671e240d
Dheeraj s <dheerajs862001@gmail.com>
2024-05-03 16:29:39 +0530

    commit

---
+++ airplane/airplane/web_form/air_ticket_web_form/air_ticket_web_form.js
@@ -0,0 +1,15 @@
+frappe.ready(function() {
+    // Add event listener for form submission
+//     frappe.web_form.events.on("after_save", function() {
+//         // Get the value of the seat field
+//         // Assuming 'seat_number' is the fieldname where the seat number is stored
+// const seatNumber = cur_frm.doc.seat_number;
+
+// // Assuming 'successMessageElement' is the DOM element where you want to display the success message
+// const successMessageElement = document.getElementById('success-message');
+
+// // Display the seat number in the success message
+// successMessageElement.innerText = `Your seat number is: ${seatNumber}`;
+
+//     });
+});
